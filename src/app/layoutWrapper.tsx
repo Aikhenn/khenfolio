361f666013b538dabe022bee7b3020ff1704083e
Aikhen <aikhenjohn23@gmail.com>
2025-10-18 15:43:12 +0800

    Fixed for iOS

--- src/app/layoutWrapper.tsx
+++ src/app/layoutWrapper.tsx
@@ -7,14 +7,14 @@ export default function LayoutWrapper({ children }: { children: React.ReactNode 
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="flex flex-col lg:flex-row box-border w-screen h-screen">
-      <div className=" top-0 p-2 left-0 h-screen  ">
+    <div className="flex flex-col lg:flex-row box-border w-screen min-h-[100dvh]">
+      <div className="p-2 w-full lg:w-auto lg:h-screen">
         <Header isOpen={isOpen} setIsOpen={setIsOpen} />
       </div>
 
-     <main
-        className={`flex-1 p-4 lg:px-12  justify-center items-center h-full transition-all duration-300`}
-      >{children}</main>
+      <main className="flex-1 min-h-0 overflow-y-auto p-4 lg:px-12 justify-center items-center transition-all duration-300">
+        {children}
+      </main>
     </div>
   );
 }
